4b4214826fab4321f25bd87adce2a2104b5e9137
januszpolak <janusz.polak@interia.pl>
2022-08-13 21:01:52 +0200

    add newsapi url to apiservices.service and import HttpClient

--- src/app/services/apiservices.service.ts
+++ src/app/services/apiservices.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiservicesService {
 
-  constructor() { }
+  constructor(private _http:HttpClient) {}
+
+    apiUrl = "https://newsapi.org/v2/top-headlines?country=pl&apiKey=8c68956f9c9a4dd083bff723b32b56fe";
+
+   
 }
